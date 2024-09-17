3710f4a68e3abc93762376ff31d04ccc3ea90eb2
Kolman-Freecss <s3rg10.martinez.roman@gmail.com>
2024-09-17 08:17:14 +0200

    Create job that triggers a pipeline with SCM

---
+++ python/02_jenkins_trigger_python.py
@@ -0,0 +1,25 @@
+import jenkins
+
+jenkins_url = 'http://localhost:8080'
+username = 'kolman'
+api_token = '1234' # Get it from Jenkins > User > Configure
+jenkins_service = jenkins.Jenkins(jenkins_url, username, api_token)
+
+# Jenkins version
+user = jenkins_service.get_whoami()
+version = jenkins_service.get_version()
+print('Hello %s from Jenkins %s' % (user['fullName'], version))
+
+job_name = 'jenkins_trigger_python'
+# --------- Job Configuration ---------
+job_config = open('.data/03_jenkins_trigger_python.xml').read()
+
+# --------- Create a new job ---------
+print(f'Creating job {job_name}')
+if jenkins_service.job_exists(job_name):
+    jenkins_service.delete_job(job_name)
+jenkins_service.create_job(job_name, job_config)
+
+# --------- Build a job ---------
+print(f'Building job {job_name}')
+jenkins_service.build_job(job_name)
---
+++ shellscript/test_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Running test script..."
+echo "Parameter 1: $1"
+echo "Parameter 2: $2"
+echo "Finished running test script."
